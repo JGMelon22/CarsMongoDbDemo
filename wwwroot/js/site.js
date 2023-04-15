344f01ab6b6131d1e5b6bc3831ba883ffa3551c1
JGMelon22 <jgvasconcellos22@gmail.com>
2023-04-15 20:47:16 -0300

    Adding JS function to hide and display loading button when loading Car Index View

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+let btnListCars = document.getElementById("CarsList");
+let btnLoading = document.getElementById("LoadingButton");
+
+btnListCars.onclick = function () {
+    btnLoading.style.display = "block"
+}
