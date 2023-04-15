40149412079b81bc950cf13d89c9d3f5be2efb03
JGMelon22 <jgvasconcellos22@gmail.com>
2023-04-15 18:28:27 -0300

    Adding Car Model

---
+++ Models/Car.cs
@@ -0,0 +1,15 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CarsMongoDbDemo.Models;
+
+public class Car
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("Brand")] public string VehicleBrand { get; set; } = string.Empty!;
+    public string Name { get; set; } = string.Empty!;
+    public int Price { get; set; }
+}
